5463b0009ab9c2ab5a13db8b3815bb75b6d64166
JanDore7 <jan_dore2030@proton.me>
2024-03-25 04:23:47 +0300

    Еще о выводах и форматах

--- main.py
+++ main.py
@@ -5,4 +5,15 @@ sl.title('Привет я приложение')
 sl.subheader('Я подзаголовок')
 sl.header('Заголовок')
 sl.text('Обычный текст')
-
+sl.markdown('**Привет** *мир*') 
+sl.markdown('[Google](https://google.com)')
+sl.caption('Подпись')
+sl.latex(r"\begin{pmatrix}a&b\\c&b\end{pmatrix}")
+json = {'a': '1, 2, 3', 'b': '4, 5 , 6'}
+sl.json(json)
+code = '''
+print('Привет')
+def funck():
+    return 0;
+'''
+sl.code(code, language='python')
