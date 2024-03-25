b3f5fe531d12c94b61ba21afead513f33ffc6292
JanDore7 <jan_dore2030@proton.me>
2024-03-25 07:03:42 +0300

    Загрузка файлов

--- main.py
+++ main.py
@@ -14,62 +14,75 @@ import pandas as pd
 
 
 
-sl.title('Привет я приложение')
-sl.subheader('Я подзаголовок')
-sl.header('Заголовок')
-sl.text('Обычный текст')
-sl.markdown('**Привет** *мир*') 
-sl.markdown('[Google](https://google.com)')
-sl.caption('Подпись')
-sl.latex(r"\begin{pmatrix}a&b\\c&b\end{pmatrix}")
-json = {'a': '1, 2, 3', 'b': '4, 5 , 6'}
-sl.json(json)
-code = '''
-print('Привет')
-def funck():
-    return 0;
-'''
-sl.code(code, language='python')
-sl.write('## H2')
-sl.write(json)
+sl.title('загрузка файлов')
+# sl.subheader('Я подзаголовок')
+# sl.header('Заголовок')
+# sl.text('Обычный текст')
+# sl.markdown('**Привет** *мир*') 
+# sl.markdown('[Google](https://google.com)')
+# sl.caption('Подпись')
+# sl.latex(r"\begin{pmatrix}a&b\\c&b\end{pmatrix}")
+# json = {'a': '1, 2, 3', 'b': '4, 5 , 6'}
+# sl.json(json)
+# code = '''
+# print('Привет')
+# def funck():
+#     return 0;
+# '''
+# sl.code(code, language='python')
+# sl.write('## H2')
+# sl.write(json)
 
-sl.metric(label='Скорость ветра', value='120ms⁻¹', delta='1.4ms⁻¹')
-sl.metric(label='Скорость ветра', value='120ms⁻¹', delta='-1.4ms⁻¹')
+# sl.metric(label='Скорость ветра', value='120ms⁻¹', delta='1.4ms⁻¹')
+# sl.metric(label='Скорость ветра', value='120ms⁻¹', delta='-1.4ms⁻¹')
 
-table = pd.DataFrame({'column 1': [1,2,3,4,5,6,7], 'column 2': [11,12,13,14,15,16,17]})
-sl.table(table)
+# table = pd.DataFrame({'column 1': [1,2,3,4,5,6,7], 'column 2': [11,12,13,14,15,16,17]})
+# sl.table(table)
 
-sl.dataframe(table)
+# sl.dataframe(table)
 
-sl.image('2024-01-10_13-29.png',caption='Снимок' , width=150, clamp=False, channels='RGB')
+# sl.image('2024-01-10_13-29.png',caption='Снимок' , width=150, clamp=False, channels='RGB')
 
-sl.audio('sample-6s.mp3')
+# sl.audio('sample-6s.mp3')
 
-sl.video('MOT - Kogda muzhchina vlyublen - 1080HD - [ VKlipe.org ].mp4')
+# sl.video('MOT - Kogda muzhchina vlyublen - 1080HD - [ VKlipe.org ].mp4')
 
-state = sl.checkbox(label='Чекбокс', value=True)
-if state:
-    sl.write('Привет')
+# state = sl.checkbox(label='Чекбокс', value=True)
+# if state:
+#     sl.write('Привет')
 
-def foo():
-    print(f'Состояние {sl.session_state.чекер}')
+# def foo():
+#     print(f'Состояние {sl.session_state.чекер}')
 
-state = sl.checkbox(label='Бокс', on_change=foo, key='чекер')
+# state = sl.checkbox(label='Бокс', on_change=foo, key='чекер')
 
-radio_btn = sl.radio('В какой стране ты живешь?', options=('US', 'RU', 'UA'))
-print(radio_btn)
+# radio_btn = sl.radio('В какой стране ты живешь?', options=('US', 'RU', 'UA'))
+# print(radio_btn)
 
-def btn():
-    print('КЛИК-КЛАК')
+# def btn():
+#     print('КЛИК-КЛАК')
 
-btn = sl.button('Нажми меня', help='кликни', on_click=btn)
-
-
-
-select = sl.selectbox('Выбери автомобиль', help='Кликни на нужный', options=('Audi', 'BMW', 'Changan', 'VAZ'))
-print(select)
+# btn = sl.button('Нажми меня', help='кликни', on_click=btn)
 
 
-m_select = sl.multiselect('Твой любимый бренд?', options=('Microsoft', 'Apple', 'Accer'))
-sl.write(m_select)
-print(m_select)
+
+# select = sl.selectbox('Выбери автомобиль', help='Кликни на нужный', options=('Audi', 'BMW', 'Changan', 'VAZ'))
+# print(select)
+
+
+# m_select = sl.multiselect('Твой любимый бренд?', options=('Microsoft', 'Apple', 'Accer'))
+# sl.write(m_select)
+# print(m_select)
+
+
+sl.markdown('---')
+images = sl.file_uploader('Загрузи изображение', type=['jpeg','png'], accept_multiple_files=True)
+if images is not None:
+    for image in images:
+        sl.image(image)
+
+
+sl.markdown('---')
+video = sl.file_uploader('Загрузи изображение', type='mp4')
+if video is not None:
+    sl.video(video)
